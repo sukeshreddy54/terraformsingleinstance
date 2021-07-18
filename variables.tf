5a94b365f9de1c2356d0666dd9b3e37bbf442459
sukeshreddy54 <77787866+sukeshreddy54@users.noreply.github.com>
2021-07-18 13:24:54 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,6 +4,7 @@ variable "aws_region" {}
 variable "amis" {
     description = "AMIs by region"
     default = {
+	    ap-south-1 = "ami-0c1a7f89451184c8b"  #ubuntu 20.04 LTS
         us-east-1 = "ami-97785bed" # ubuntu 14.04 LTS
 		us-east-2 = "ami-f63b1193" # ubuntu 14.04 LTS
 		us-west-1 = "ami-824c4ee2" # ubuntu 14.04 LTS
@@ -25,13 +26,13 @@ variable "private_subnet_name" {}
 variable Main_Routing_Table {}
 variable "azs" {
   description = "Run the EC2 Instances in these Availability Zones"
-  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  default = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
 }
 variable "environment" { default = "dev" }
 variable "instance_type" {
   default = {
-    dev = "t2.nano"
-    test = "t2.micro"
+    dev = "t2.micro"
+    test = "t2.nano"
     prod = "t2.medium"
     }
 }
